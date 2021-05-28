dc03c9e8696280738fb4393378cb6ce4525c1f2d
Timo Welde <tjwelde@gmail.com>
2021-05-28 17:31:57 +0200

    get accounts from wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromAddress,
+  web3FromSource,
+  web3ListRpcProviders,
+  web3UseRpcProvider,
+} from '@polkadot/extension-dapp'
+import type { InjectedAccountWithMeta } from '@polkadot/extension-inject/types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import { connect } from './utils'
+
+async function getGenesis() {
+  const api = await connect()
+
+  console.log(api.genesisHash.toHex())
 }
 
-export default App;
+async function getAllAccounts() {
+  const allInjected = await web3Enable('KILT Staking App')
+  console.log('allInjected', allInjected)
+  const allAccounts = await web3Accounts()
+  console.log('allAccounts', allAccounts)
+  const source = allAccounts[0].meta.source
+  console.log('source', source)
+  const injector = await web3FromSource(source)
+  console.log('injector', injector)
+  const rpcProviders = await web3ListRpcProviders(source)
+  console.log('rpcProviders', rpcProviders)
+}
+
+getAllAccounts()
+
+getGenesis()
+
+function App() {
+  const [web3Enabled, setWeb3Enabled] = useState(false)
+  const [allAccounts, setAllAccounts] = useState<InjectedAccountWithMeta[]>([])
+
+  useEffect(() => {
+    async function doEffect() {
+      const allInjected = await web3Enable('KILT Staking App')
+      setWeb3Enabled(true)
+    }
+    doEffect()
+  }, [])
+
+  useEffect(() => {
+    async function doEffect() {
+      if (web3Enabled) {
+        const allAccounts = await web3Accounts()
+        setAllAccounts(allAccounts)
+      }
+    }
+    doEffect()
+  }, [web3Enabled])
+
+  return (
+    <div className="App">
+      <ul>
+        {allAccounts.map((account) => (
+          <li>{account.meta.name} - {account.address}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+import { ApiPromise, WsProvider } from '@polkadot/api'
+
+let cachedApi: ApiPromise | null = null;
+
+export async function connect() {
+  if (!cachedApi) {
+    const wsProvider = new WsProvider('wss://peregrine.kilt.io')
+    cachedApi = await ApiPromise.create({ provider: wsProvider })
+  }
+  if (!cachedApi.isConnected) {
+    await cachedApi.connect()
+  }
+  return cachedApi
+}
+
+export function disconnect() {
+  return cachedApi?.disconnect()
+}
