ab6cdfed6a9c8477fb9022dc1c13aaa11d05143b
Timo Welde <tjwelde@gmail.com>
2021-06-22 19:01:38 +0200

    feat: move CollatorRow

--- src/components/CollatorListRow/CollatorListRow.tsx
+++ src/components/CollatorListRow/CollatorListRow.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
-import cx from 'classnames'
-import { Collator } from '../Collator/Collator'
-import { Button } from '../Button/Button'
-import { Icon } from '../Icon/Icon'
 import { Data } from '../../types'
 import styles from './CollatorListRow.module.css'
 import rowStyles from '../../styles/row.module.css'
-import { format, leftFillZero } from '../../utils'
 import { StakeRow } from '../StakeRow/StakeRow'
 import { NewStakeRow } from '../NewStakeRow/NewStakeRow'
+import { CollatorRow } from '../CollatorRow/CollatorRow'
 
 export interface Props {
   entry: Data
@@ -22,59 +18,15 @@ export const CollatorListRow: React.FC<Props> = ({ entry, rank }) => {
   return (
     <>
       <tr className={styles.firstRow}></tr>
-      <tr
-        className={cx({
-          [styles.row]: true,
-          [rowStyles.staked]: entry.stakes.length,
-        })}
-      >
-        <td className={rowStyles.spacer}></td>
-        <td>
-          <Button>
-            <Icon type="fav_yellow" />
-          </Button>
-          <Collator address={entry.collator} />
-        </td>
-        <td>
-          {entry.active ? (
-            <Icon type="bulb_yellow" />
-          ) : (
-            <Icon type="bulb_gray" />
-          )}
-          {entry.activeNext ? (
-            <Icon type="next_session_yellow" />
-          ) : (
-            <Icon type="next_session_gray" />
-          )}
-        </td>
-        <td>
-          {format(entry.stake)} ({leftFillZero(rank, 3)})
-        </td>
-        <td></td>
-        <td>{leftFillZero(entry.delegators, 2)} / 25</td>
-        <td>{format(entry.lowestStake)}</td>
-        <td>
-          {entry.stakes.length ? (
-            <Icon type="tokens_yellow" />
-          ) : (
-            <Button
-              onClick={() => {
-                setExpanded(!expanded)
-              }}
-            >
-              <Icon type="tokens_gray" />
-            </Button>
-          )}
-        </td>
-        <td className={rowStyles.spacer}></td>
-      </tr>
+      <CollatorRow
+        entry={entry}
+        rank={rank}
+        expanded={expanded}
+        setExpanded={setExpanded}
+      />
       {entry.stakes.length > 0 &&
-        entry.stakes.map((stakeInfo) => (
-          <StakeRow stakeInfo={stakeInfo} />
-        ))}
-      {expanded && (
-        <NewStakeRow />
-      )}
+        entry.stakes.map((stakeInfo) => <StakeRow stakeInfo={stakeInfo} />)}
+      {expanded && <NewStakeRow />}
       <tr className={styles.lastRow}>
         <td className={rowStyles.spacer}></td>
         <td colSpan={COLS}></td>
---
+++ src/components/CollatorRow/CollatorRow.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import cx from 'classnames'
+import { Collator } from '../Collator/Collator'
+import { Button } from '../Button/Button'
+import { Icon } from '../Icon/Icon'
+import rowStyles from '../../styles/row.module.css'
+import { format, leftFillZero } from '../../utils'
+import { Data } from '../../types'
+
+export interface Props {
+  entry: Data
+  rank: number | undefined
+  setExpanded: (expanded: boolean) => void
+  expanded: boolean
+}
+
+export const CollatorRow: React.FC<Props> = ({ entry, rank, setExpanded, expanded }) => {
+  return (
+    <tr
+      className={cx({
+        [rowStyles.row]: true,
+        [rowStyles.staked]: entry.stakes.length,
+      })}
+    >
+      <td className={rowStyles.spacer}></td>
+      <td>
+        <Button>
+          <Icon type="fav_yellow" />
+        </Button>
+        <Collator address={entry.collator} />
+      </td>
+      <td>
+        {entry.active ? <Icon type="bulb_yellow" /> : <Icon type="bulb_gray" />}
+        {entry.activeNext ? (
+          <Icon type="next_session_yellow" />
+        ) : (
+          <Icon type="next_session_gray" />
+        )}
+      </td>
+      <td>
+        {format(entry.stake)} ({leftFillZero(rank, 3)})
+      </td>
+      <td></td>
+      <td>{leftFillZero(entry.delegators, 2)} / 25</td>
+      <td>{format(entry.lowestStake)}</td>
+      <td>
+        {entry.stakes.length ? (
+          <Icon type="tokens_yellow" />
+        ) : (
+          <Button
+            onClick={() => {
+              setExpanded(!expanded)
+            }}
+          >
+            <Icon type="tokens_gray" />
+          </Button>
+        )}
+      </td>
+      <td className={rowStyles.spacer}></td>
+    </tr>
+  )
+}
