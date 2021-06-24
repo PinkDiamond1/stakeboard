66f5ff7e67dbf411c543b36948c0ae30cf853e99
Timo Welde <tjwelde@gmail.com>
2021-06-24 19:00:46 +0200

    fix: connect to blockchain

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -1,11 +1,12 @@
 import { ApiPromise, WsProvider } from '@polkadot/api'
+import { types10 as types } from '@kiltprotocol/type-definitions'
 
-let cachedApi: ApiPromise | null = null;
+let cachedApi: ApiPromise | null = null
 
 export async function connect() {
   if (!cachedApi) {
     const wsProvider = new WsProvider('wss://kilt-peregrine-k8s.kilt.io')
-    cachedApi = await ApiPromise.create({ provider: wsProvider })
+    cachedApi = await ApiPromise.create({ provider: wsProvider, types })
   }
   if (!cachedApi.isConnected) {
     await cachedApi.connect()
