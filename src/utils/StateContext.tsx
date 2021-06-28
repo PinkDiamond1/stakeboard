bd2f6ad46b0d60e72115c1f3ba1b584b83046962
Timo Welde <tjwelde@gmail.com>
2021-06-28 17:53:07 +0200

    feat: save state in localstorage

--- src/utils/StateContext.tsx
+++ src/utils/StateContext.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, useReducer } from 'react'
+import React, { Dispatch, useEffect, useReducer } from 'react'
 import { FavoriteActions, favoriteReducer } from '../state/reducers'
 
 export interface State {
@@ -15,7 +15,27 @@ const mainReducer = ({ favorites }: State, action: FavoriteActions) => ({
 })
 
 export const StateProvider: React.FC = ({ children }) => {
-  const [state, dispatch] = useReducer(mainReducer, { favorites: [] })
+  const [state, dispatch] = useReducer(
+    mainReducer,
+    { favorites: [] },
+    (initialArg) => {
+      try {
+        const item = window.localStorage.getItem('staking-state')
+        return item ? JSON.parse(item) : initialArg
+      } catch (err) {
+        console.log(err)
+        return initialArg
+      }
+    }
+  )
+
+  useEffect(() => {
+    try {
+      window.localStorage.setItem('staking-state', JSON.stringify(state))
+    } catch (err) {
+      console.log(err)
+    }
+  }, [state])
 
   return (
     <StateContext.Provider value={{ state, dispatch }}>
