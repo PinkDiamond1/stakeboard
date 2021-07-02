c849a2e39796dbe4e6d81a6fbecfcf3bf844dbc0
Timo Welde <tjwelde@gmail.com>
2021-07-02 17:22:12 +0200

    feat: remove separator when not needed

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,18 +3,19 @@ import styles from './Dashboard.module.css'
 import cx from 'classnames'
 
 export interface Account {
-  staked: number,
+  staked: number
   stakeable: number
 }
 
 const accounts: Account[] = [
   {
-    staked: 1_000,
+    staked: 4_000,
     stakeable: 10_000,
-  },{
-    staked: 1_000,
+  },
+  {
+    staked: 9_000,
     stakeable: 100_000,
-  }
+  },
 ]
 
 const divisor = 100
@@ -36,14 +37,24 @@ const BarItem: React.FC<BarItemProps> = ({ style, amount }) => {
 export interface TokenBarProps {
   account: Account
 }
-export const TokenBar: React.FC<TokenBarProps> = ({account}) => {
+export const TokenBar: React.FC<TokenBarProps> = ({ account }) => {
   const { staked, stakeable } = account
   const total = staked + stakeable
+
+  const has_staked = staked > 0
+  const has_stakeable = stakeable > 0
+
+  let container_width = total / divisor
+  container_width =
+    has_staked && has_stakeable ? container_width + 1 : container_width
+
   return (
-    <div className={styles.container} style={{ width: total / divisor + 1 }}>
-      <BarItem style={styles.staked} amount={staked} />
-      <div className={cx(styles.item, styles.separator)} />
-      <BarItem style={styles.stakeable} amount={stakeable} />
+    <div className={styles.container} style={{ width: container_width }}>
+      {has_staked && <BarItem style={styles.staked} amount={staked} />}
+      {has_staked && has_stakeable && (
+        <div className={cx(styles.item, styles.separator)} />
+      )}
+      {has_stakeable && <BarItem style={styles.stakeable} amount={stakeable} />}
     </div>
   )
 }
@@ -52,9 +63,9 @@ export interface Props {}
 export const Dashboard: React.FC<Props> = () => {
   return (
     <div className={styles.dashboard}>
-      {accounts.map(account => (
+      {accounts.map((account) => (
         <span className={styles.account}>
-          <TokenBar account={account}/>
+          <TokenBar account={account} />
         </span>
       ))}
     </div>
