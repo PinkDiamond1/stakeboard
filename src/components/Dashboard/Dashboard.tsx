749f1291dbf692edf15eddd87c556ac88949810f
Timo Welde <tjwelde@gmail.com>
2021-07-05 11:26:54 +0200

    feat: up/down account info

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -43,8 +43,12 @@ const BarItem: React.FC<BarItemProps> = ({ style, amount }) => {
 
 export interface TokenBarProps {
   account: Account
+  down?: boolean
 }
-export const TokenBar: React.FC<TokenBarProps> = ({ account }) => {
+export const TokenBar: React.FC<TokenBarProps> = ({
+  account,
+  down = false,
+}) => {
   const { staked, stakeable } = account
   const total = staked + stakeable
 
@@ -56,7 +60,10 @@ export const TokenBar: React.FC<TokenBarProps> = ({ account }) => {
     has_staked && has_stakeable ? container_width + 1 : container_width
 
   return (
-    <div className={styles.container} style={{ width: container_width }}>
+    <div
+      className={cx(styles.container, { [styles.containerDown]: down })}
+      style={{ width: container_width }}
+    >
       {has_staked && <BarItem style={styles.staked} amount={staked} />}
       {has_staked && has_stakeable && (
         <div className={cx(styles.item, styles.separator)} />
@@ -66,25 +73,51 @@ export const TokenBar: React.FC<TokenBarProps> = ({ account }) => {
   )
 }
 
+export interface MetaProps {
+  account: Account
+}
+export const MetaUp: React.FC<MetaProps> = ({ account }) => {
+  return (
+    <div className={styles.meta}>
+      <div className={styles.identicon}>
+        <Identicon address={account.address} />
+        <div className={styles.line}></div>
+      </div>
+      <div className={styles.info}>
+        <div>{account.name}</div>
+        <div>MY STAKE: 100,000.00 KILT</div>
+        <div>STAKEABLE: 100,000.00 KILT</div>
+      </div>
+    </div>
+  )
+}
+
+export const MetaDown: React.FC<MetaProps> = ({ account }) => {
+  return (
+    <div className={styles.metaDown}>
+      <div className={cx(styles.identicon, styles.identiconDown)}>
+        <div className={cx(styles.line, styles.lineDown)}></div>
+        <Identicon address={account.address} />
+      </div>
+      <div className={styles.info}>
+        <div>{account.name}</div>
+        <div>MY STAKE: 100,000.00 KILT</div>
+        <div>STAKEABLE: 100,000.00 KILT</div>
+      </div>
+    </div>
+  )
+}
+
 export interface Props {}
 export const Dashboard: React.FC<Props> = () => {
   return (
     <div className={styles.dashboard}>
-      {accounts.map((account) => (
+      {accounts.map((account, index) => (
         <>
           <span className={styles.account}>
-            <div className={styles.meta}>
-              <div className={styles.identicon}>
-                <Identicon address={account.address} />
-                <div className={styles.line}></div>
-              </div>
-              <div className={styles.info}>
-                <div>{account.name}</div>
-                <div>MY STAKE: 100,000.00 KILT</div>
-                <div>STAKEABLE: 100,000.00 KILT</div>
-              </div>
-            </div>
-            <TokenBar account={account} />
+            {index % 2 === 0 && <MetaUp account={account} />}
+            <TokenBar account={account} down={index % 2 !== 0} />
+            {index % 2 !== 0 && <MetaDown account={account} />}
           </span>
         </>
       ))}
