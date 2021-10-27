efa0c1af8fc07a0a7c6bb7e71bdef972d458d4b6
Timo Welde <tjwelde@gmail.com>
2021-10-27 13:15:30 +0200

    chore: update dependencies to work with kilt 1.0.0 (#50)
    * chore: update dependencies to work with kilt 1.0.0
    
    * chore: remove unused subscriber functions

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -17,27 +17,6 @@ export async function getGenesis() {
   return api.genesisHash.toHex()
 }
 
-export async function subscribeToCandidatePool(
-  listener: (result: Vec<ChainTypes.Stake>) => void
-) {
-  const api = await getConnection()
-  // @ts-ignore
-  window.trigger = (pool) => {
-    listener(pool)
-  }
-  api.query.parachainStaking.candidatePool<Vec<ChainTypes.Stake>>(listener)
-}
-
-export async function subscribeToCollatorState(
-  account: string,
-  listener: (result: Option<ChainTypes.Collator>) => void
-) {
-  const api = await getConnection()
-  return await api.query.parachainStaking.collatorState<
-    Option<ChainTypes.Collator>
-  >(account, listener)
-}
-
 export async function getCandidatePool() {
   const api = await getConnection()
   return api.query.parachainStaking.candidatePool.entries<
@@ -140,11 +119,9 @@ export async function signAndSend(
         if (dispatchError.isModule) {
           // for module errors, we have the section indexed, lookup
           const decoded = api.registry.findMetaError(dispatchError.asModule)
-          const { documentation, name, section } = decoded
+          const { docs, name, section } = decoded
 
-          const error = new Error(
-            `${section}.${name}: ${documentation.join(' ')}`
-          )
+          const error = new Error(`${section}.${name}: ${docs.join(' ')}`)
           onError(error)
         } else {
           // Other, CannotLookup, BadOrigin, no extra info
