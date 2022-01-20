729793bd99fd4a1adcaca3a01efcb5ba8528a7e5
Skedley <48058984+Dudleyneedham@users.noreply.github.com>
2022-01-20 11:59:32 +0100

    feat: updating header (#58)
    * feat: updating hearder
    
    * feat: updating the svg
    
    * feat: updating the sizeing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,11 @@ import styles from './Header.module.css'
 export const Header: React.FC = () => {
   return (
     <div className={styles.header}>
-      <Icon type="KILT_logo" width={50} />
-      <span className={styles.spacer} />
-      <span className={styles.name}>Stakeboard</span>
-      <Icon type="skateboarder" width={60} />
+      <div>
+        <span className={styles.name}>Stakeboard</span>
+        <Icon type="skateboarder" width={60} />
+      </div>
+      <Icon type="BUILT_ON_KILT" width={70} />
     </div>
   )
 }
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -15,6 +15,7 @@ import exclamation from '../../icons/exclamation.svg'
 import fav_gray from '../../icons/fav_gray.svg'
 import fav_yellow from '../../icons/fav_yellow.svg'
 import KILT_logo from '../../icons/KILT_logo.svg'
+import BUILT_ON_KILT from '../../icons/Built_on_KILT.svg'
 import next_session_gray from '../../icons/next_session_gray.svg'
 import next_session_yellow from '../../icons/next_session_yellow.svg'
 import order_white from '../../icons/order_white.svg'
@@ -45,6 +46,7 @@ const Icons = {
   block,
   bulb_gray,
   bulb_yellow,
+  BUILT_ON_KILT,
   check,
   close,
   exclamation_new,
