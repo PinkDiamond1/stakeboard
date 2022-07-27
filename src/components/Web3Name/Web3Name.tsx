6cdd6a26ce0cde1b90fad73b145a815515cc895f
weichweich <albrecht@kilt.io>
2022-07-27 15:06:35 +0200

    feat: add link to w3n.id for w3ns

---
+++ src/components/Web3Name/Web3Name.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './Web3Name.module.css'
+
+const W3N_URL = process.env.W3N_URL || 'https://w3n.id/'
+const W3N_PREFIX = process.env.W3N_PREFIX || 'w3n:'
+
+export interface Props {
+  name: string
+}
+
+export const Web3Name: React.FC<Props> = ({ name }) => {
+  return (
+    <>
+      <a
+        className={styles.yellow}
+        href={W3N_URL + name}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        {W3N_PREFIX + name}
+      </a>
+    </>
+  )
+}
